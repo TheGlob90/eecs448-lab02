978204031c067d00bc2644107191ce235f1e1d0e
Brandon Lefert <brandon.lefert@gmail.com>
2021-02-12 12:22:48 -0600

    Fixed search(), score is now 100

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -47,10 +47,13 @@ bool LinkedList<T>::search(T value) const
 				if (temp->getValue() == value)
 				{
 					isFound = true;
-					return(isFound);
 				}
 				temp = temp->getNext();
 	  }
+		if (temp->getValue() == value)
+		{
+			isFound = true;
+		}
 	}
 
 	return(isFound);
